c4ef63d6b278f1eddf35ce7c307c33e6163f727a
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-03-31 02:07:01 +0300

    add variables for backend logic

---
+++ index.js
@@ -0,0 +1,22 @@
+let displayInput = document.getElementById("input")
+
+let demo = document.getElementById('demo')
+
+let metersConversion = 3.281
+
+let feetConversion = 0.304
+
+let litresConversion = 0.264
+
+let gallonsConversion = 3.785
+
+let kilogramsConversion = 2.205
+
+let poundsConversion = 0.454
+
+let input = parseInt(displayInput.value)
+
+function functionOne(){
+
+    console.log(typeof(input))
+}
