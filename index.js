e1c8dc03f73cf19cc1823cecafd5cfac977cdef6
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-03-31 02:50:57 +0300

    bugs

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 let displayInput = document.getElementById("input")
 
+
+let metersleft = document.getElementById("metersDisplayLeft")
+
+let metersRight = document.getElementById("metersDisplayRight")
+
+let feetLeft = document.getElementById("feetDisplayLeft")
+
+let feetRight = document.getElementById("feetDisplayRight")
+
+let litresLeft = document.getElementById("litresDisplayLeft")
+
+let litresRight = document.getElementById("litresDisplayRight")
+
+let gallonsLeft = document.getElementById("gallonsDisplayLeft")
+
+let gallonsRight = document.getElementById("gallonsDisplayRight")
+
+let kilosLeft = document.getElementById("kilosDisplayLeft")
+
+let kilosRight = document.getElementById("kilosDisplayRight")
+
+let poundsLeft = document.getElementById("poundsDisplayLeft")
+
+let poundsRight = document.getElementById("poundsDisplayRight")
+
 let demo = document.getElementById('demo')
 
 let metersConversion = 3.281
@@ -14,9 +39,53 @@ let kilogramsConversion = 2.205
 
 let poundsConversion = 0.454
 
-let input = parseInt(displayInput.value)
 
-function functionOne(){
+// let input = parseInt(displayInput.value)
 
-    console.log(typeof(input))
+
+function functionOne() {
+
+    metersleft.textContent = parseInt(displayInput.value)
+
+    let sum1 = parseInt(displayInput.value) * 3.281
+
+    feetLeft.textContent = sum1
+
+    feetRight.textContent = parseInt(displayInput.value)
+
+    let sum2 = parseInt(displayInput.value) * 0.304
+
+    metersRight.textContent = sum2
+
+    ///////
+
+    litresLeft.textContent = parseInt(displayInput.value)
+
+    let sum3 = parseInt(displayInput.value) * 0.264
+
+    gallonsLeft.textContent = sum3
+
+    litresRight.textContent = parseInt(displayInput.value)
+
+    let sum4 = parseInt(displayInput.value) * 3.785
+
+    gallonsRight.textContent = sum4
+
+    ///////
+
+    kilosLeft.textContent = parseInt(displayInput.value)
+
+    let sum5 = parseInt(displayInput.value) * 2.205
+
+    poundsLeft.textContent = sum5
+
+    kilosRight.textContent = parseInt(displayInput.value)
+
+    let sum6 = parseInt(displayInput.value) * 0.454
+
+    poundsRight.textContent = sum6
+
+
+    ///////
+
 }
